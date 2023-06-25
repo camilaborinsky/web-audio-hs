63ebfcad96c6eee3a3b17df56b8f099cc4337eca
Gonzalo Arca <goarca@itba.edu.ar>
2023-06-25 18:24:01 -0300

    Add node type as label in compiled DOT

--- src/WebAudioApiGraphDrawer.hs
+++ src/WebAudioApiGraphDrawer.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ImportQualifiedPost #-}
 
 module WebAudioApiGraphDrawer where
 
@@ -6,21 +7,20 @@ import Data.GraphViz
 import Data.GraphViz.Printing (PrintDot (..), unqtText)
 import Data.GraphViz.Types.Canonical
 import Data.GraphViz.Types.Generalised (fromGeneralised)
-import qualified Data.GraphViz.Types.Generalised as G
+import Data.GraphViz.Types.Generalised qualified as G
 import Data.GraphViz.Types.Graph (toCanonical)
 import Data.GraphViz.Types.Monadic
 import Data.Text.Lazy
 import System.FilePath
 import WebAudioMonad
--- import Text.PrettyPrint.Leijen.Text (text, (<+>), brackets)
 
 instance PrintDot AudioNodeVar where
-  unqtDot (AudioNodeVar (audioNode, var)) = unqtText (pack var)
-    --  text (pack var) <+> brackets (toDot $ textLabel $ pack $ show audioNode ++ " " ++ var)
-    -- let 
-    --     varLabel = textLabel $ pack var
-    --     dotNode = DotNode var []
-    -- in unqtDot dotNode
+  unqtDot (AudioNodeVar (audioNode, var)) = unqtText $ pack $ var ++ " [label=" ++ show audioNode ++ "]"
+
+-- let
+--     varLabel = textLabel $ pack var
+--     dotNode = DotNode var []
+-- in unqtDot dotNode
 
 newtype WebAudioApiGraphDrawer a = WebAudioApiGraphDrawer {runWebAudioApiGraphDrawer :: DotM AudioNodeVar a}
   deriving (Functor, Applicative, Monad)
@@ -30,19 +30,15 @@ instance WebAudioMonad WebAudioApiGraphDrawer where
     graphNode <- node audioNodeVar []
     return audioNodeVar
 
-
-  execute = createImage  . digraph' . runWebAudioApiGraphDrawer
+  execute = createImage . digraph' . runWebAudioApiGraphDrawer
 
 -- drawGraph :: PrintDotRepr dg n => dg n -> IO FilePath
 
-createImage :: PrintDotRepr dg n =>  dg n -> IO FilePath
-createImage = createImageInDir "./tmp" "ex1" Png 
+createImage :: PrintDotRepr dg n => dg n -> IO FilePath
+createImage = createImageInDir "./tmp" "ex1" Png
 
 createImageInDir :: PrintDotRepr dg n => FilePath -> FilePath -> GraphvizOutput -> dg n -> IO FilePath
 createImageInDir d n o g = Data.GraphViz.addExtension (runGraphvizCommand Dot g) o (combine d n)
 
-
 executeString :: WebAudioApiGraphDrawer a -> [Char]
-executeString = unpack . printDotGraph . (fromGeneralised :: G.DotGraph AudioNodeVar -> DotGraph AudioNodeVar) . digraph' . runWebAudioApiGraphDrawer 
-
-
+executeString = unpack . printDotGraph . (fromGeneralised :: G.DotGraph AudioNodeVar -> DotGraph AudioNodeVar) . digraph' . runWebAudioApiGraphDrawer
--- src/WebAudioMonad.hs
+++ src/WebAudioMonad.hs
@@ -11,10 +11,15 @@ data AudioNode
 
 newtype AudioNodeVar = AudioNodeVar (AudioNode, [Char])
 
+instance Show AudioNode where
+  show OscillatorNode = "OscillatorNode"
+  show (GainNode gain) = "GainNode " ++ show gain
+  show (DelayNode delay) = "DelayNode " ++ show delay
+
 instance Eq AudioNodeVar where
   (AudioNodeVar (_, varName1)) == (AudioNodeVar (_, varName2)) = varName1 == varName2
 
-instance Ord AudioNodeVar where 
+instance Ord AudioNodeVar where
   compare (AudioNodeVar (_, varName1)) (AudioNodeVar (_, varName2)) = compare varName1 varName2
 
 class Monad m => WebAudioMonad m where
@@ -26,4 +31,4 @@ class Monad m => WebAudioMonad m where
   createNode :: AudioNodeVar -> m AudioNodeVar
 
   -- TODO: HACER MAS GENERICA, O ASEGURARSE QUE EL GRAFO TMB SEA [Char]
-  execute :: m a  -> IO FilePath
+  execute :: m a -> IO FilePath
