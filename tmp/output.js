9a73919fb08d114a28f18a60f0cce459e5b79a6b
Gonzalo Arca <goarca@itba.edu.ar>
2023-07-02 23:42:08 -0300

    Remove tmp folder

--- tmp/output.js
+++
@@ -1,7 +0,0 @@
-const audioContext = new AudioContext();
-const oscillator1 = new OscillatorNode(audioContext, {"detune":1.5,"frequency":0.5,"type":"sine"});
-const oscillator2 = new OscillatorNode(audioContext, {"detune":0.1,"frequency":0.5,"type":"sine"});
-oscillator1.frequency.value = 440.0;
-const detune = oscillator1.detune;
-oscillator2.detune.value = 1.5;
-oscillator1.connect(oscillator2);
